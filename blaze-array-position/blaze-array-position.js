ca21e134a6bcc37282d9d719d93da2e8964b15f2
hwillson <hugh@octonary.com>
2016-03-10 11:05:11 -0500

    Wired up blaze array position test.

--- blaze-array-position/blaze-array-position.js
+++ blaze-array-position/blaze-array-position.js
@@ -1,24 +1,29 @@
+const imgData = [
+  {
+    img_source: '../images/earthElemental.png',
+    alt: 'earth elemental'
+  },
+  {
+    img_source: '../images/elementalAirPNGI.png',
+    alt: 'air elemental'
+  },
+  {
+    img_source: '../images/fireElemental.png',
+    alt: 'fire elemental'
+  },
+  {
+    img_source: '../images/waterElemental.png',
+    alt: 'earth elemental'
+  }
+];
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    imgData() {
-      return [
-        {
-          img_source: '../images/earthElemental.png',
-          alt: 'earth elemental'
-        },
-        {
-          img_source: '../images/elementalAirPNGI.png',
-          alt: 'air elemental'
-        },
-        {
-          img_source: '../images/fireElemental.png',
-          alt: 'fire elemental'
-        },
-        {
-          img_source: '../images/waterElemental.png',
-          alt: 'earth elemental'
-        }
-      ];
+    getImage(position) {
+      return imgData[position];
+    },
+    getImageSource(position) {
+      return imgData[position].img_source;
     }
   });
 }
